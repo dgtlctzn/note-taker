e2ac88011c37c527ad156f73bb43acf27eaabe85
Joseph Perry <josephperry720@gmail.com>
2020-09-30 16:26:26 -0400

    added server js route mapping code

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`app listening on PORT: ${PORT}`)
+})
+
