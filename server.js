01bc6825dce29da9be4150b534142ac8a700a771
Joseph Perry <josephperry720@gmail.com>
2020-09-30 17:19:05 -0400

    fixed html routes

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
-require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes.js')(app);
+// require('./routes/apiRoutes.js')(app);
 
-app.listen(PORT, () => {
+app.listen(PORT, function() {
     console.log(`app listening on PORT: ${PORT}`)
 })
 
