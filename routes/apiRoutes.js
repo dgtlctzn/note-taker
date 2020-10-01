063b6800e4fd70795ffce99f9b34a928b6361068
Joseph Perry <josephperry720@gmail.com>
2020-10-01 15:48:22 -0400

    added post method

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,21 +2,60 @@ const path = require("path");
 const fs = require("fs");
 
 module.exports = function (app) {
-
   app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, jsonData) => {
-      if (err) {
-        return res.status(500).json({
-          error: true,
-          data: null,
-          message: "unable to find info",
+    fs.readFile(
+      path.join(__dirname, "../db/db.json"),
+      "utf-8",
+      (err, jsonData) => {
+        if (err) {
+          return res.status(500).json({
+            error: true,
+            data: null,
+            message: "unable to find info",
+          });
+        }
+        res.json({
+          error: false,
+          data: JSON.parse(jsonData),
+          message: "information returned",
         });
       }
-      res.json({
-        error: false,
-        data: JSON.parse(jsonData),
-        message: "information returned",
-      });
-    });
+    );
+  });
+
+  app.post("/api/notes", (req, res) => {
+    fs.readFile(
+      path.join(__dirname, "../db/db.json"),
+      "utf-8",
+      (err, jsonData) => {
+        if (err) {
+          return res.status(500).json({
+            error: true,
+            data: null,
+            message: "unable to retrieve notes.",
+          });
+        }
+        const updateData = JSON.parse(jsonData);
+        updateData.push(req.body);
+        fs.writeFile(
+          path.join(__dirname, "../db/db.json"),
+          JSON.stringify(updateData),
+          (err) => {
+            if (err) {
+              return res.status(400).json({
+                error: true,
+                data: null,
+                message: "unable to write to file",
+              });
+            }
+            res.json({
+              error: false,
+              data: updateData,
+              message: "information returned",
+            });
+          }
+        );
+      }
+    );
   });
 };
