57e0df083d39170e1e7fd86d53ed931318f0567c
Joseph Perry <josephperry720@gmail.com>
2020-10-01 16:33:48 -0400

    added app get :id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -38,9 +38,9 @@ module.exports = function (app) {
         const updateData = JSON.parse(jsonData);
 
         if (updateData.length > 0) {
-            req.body.id = updateData[updateData.length - 1].id + 1;
+          req.body.id = updateData[updateData.length - 1].id + 1;
         } else {
-            req.body.id = 1
+          req.body.id = 1;
         }
 
         updateData.push(req.body);
@@ -64,5 +64,29 @@ module.exports = function (app) {
         );
       }
     );
+  });
+
+  app.get("/api/notes/:id", (req, res) => {
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
+      if (err) {
+        return res.status(500).json({
+          error: true,
+          data: null,
+          message: "unable to retrieve notes.",
+        });
+      }
+      let id = parseInt(req.params.id);
+      const noteData = JSON.parse(data);
+      for (const note of noteData) {
+        if (id === note.id) {
+          return res.json({
+            error: false,
+            data: note,
+            message: "information returned",
+          });
+        }
+      }
+      return res.json({ error: true, data: null, message: "invalid id" });
+    });
   });
 };
